2ee31c4a1441c747ed02ad282169b9abec0e2009
Rochelle Rothman <Rothman.Rochelle@gmail.com>
2026-09-25 21:34:48 +0200

    Add missingness visualization task

--- data_analysis/data_preparation_visualization/1-plot_missingness.py
+++ data_analysis/data_preparation_visualization/1-plot_missingness.py
@@ -6,14 +6,23 @@ import numpy as np
 
 
 def plot_missingness(df):
-    """Visualize missing values in a DataFrame using a scatter plot."""
+    """Visualize missing values in a DataFrame."""
     plt.figure(figsize=(12, 8))
 
-    missing = df.isnull().to_numpy()
-    rows, columns = np.where(missing)
+    missing = df.isnull()
 
-    plt.scatter(rows, columns, marker='|')
-    plt.yticks(np.arange(len(df.columns)), df.columns)
+    for i, column in enumerate(df.columns):
+        rows = np.where(missing[column])[0]
+        plt.scatter(
+            rows,
+            np.full(len(rows), i),
+            marker='|'
+        )
+
+    plt.yticks(
+        np.arange(len(df.columns)),
+        df.columns
+    )
 
     plt.tight_layout()
     plt.show()
